b692279a532f41c4c0b6af9c8f17c645acd47d84
Serg T <sergtsaeb@gmail.com>
2017-04-03 15:45:39 -0700

    Foundation extensions for UserDefaults

---
+++ Github-Client/Github-Client/FoundationExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  FoundationExtensions.swift
+//  Github-Client
+//
+//  Created by Sergelenbaatar Tsogtbaatar on 4/3/17.
+//  Copyright © 2017 Serg Tsogtbaatar. All rights reserved.
+//
+
+import Foundation
